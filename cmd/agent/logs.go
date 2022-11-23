4c97dc1907c80de983ad461b9ed5b439ced01e37
XiovV <xiovv@tutanota.com>
2022-11-23 14:00:56 +0100

    cleanup

--- cmd/agent/logs.go
+++ cmd/agent/logs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"github.com/XiovV/centralog-agent/docker"
 	pb "github.com/XiovV/centralog-agent/grpc"
 	"github.com/docker/docker/api/types"
@@ -10,8 +11,10 @@ import (
 )
 
 func (s *Server) GetLogs(request *pb.GetLogsRequest, stream pb.Centralog_GetLogsServer) error {
-	if len(request.Containers) == 0 {
-		return status.Error(codes.InvalidArgument, "containers array empty")
+	err := s.validateGetLogsRequest(request)
+
+	if err != nil {
+		return status.Error(codes.InvalidArgument, err.Error())
 	}
 
 	s.LogBuffer.Flush()
@@ -19,39 +22,59 @@ func (s *Server) GetLogs(request *pb.GetLogsRequest, stream pb.Centralog_GetLogs
 	options := types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true, Timestamps: true}
 
 	if request.GetFollow() {
-		options.Follow = true
-
-		if !request.ShowAll {
-			options.Since = "0m"
-		}
-
-		stopSignals := []chan struct{}{}
-
-		var wg sync.WaitGroup
-
-		wg.Add(1)
-		for _, container := range request.Containers {
-			stopSignal := make(chan struct{})
-			stopSignals = append(stopSignals, stopSignal)
-
-			logWriter := docker.NewServerLogWriter(stopSignal, container, stream)
-
-			go s.Docker.CollectLogs(container, logWriter, options)
-		}
-
-		// since the logs are being collected inside goroutines, we have to wait until
-		// the client closes the connection so the connection doesn't get instantly
-		// dropped by the server.
-		select {
-		case <-stream.Context().Done():
-			for _, signal := range stopSignals {
-				signal <- struct{}{}
-			}
-			wg.Done()
-		}
+		wg := new(sync.WaitGroup)
+		s.followLogs(wg, options, stream, request)
 
 		wg.Wait()
+		return nil
 	}
 
 	return nil
+}
+
+func (s *Server) validateGetLogsRequest(request *pb.GetLogsRequest) error {
+	if len(request.Containers) == 0 {
+		return errors.New("containers array empty")
+	}
+
+	if request.GetFirst() > 0 && request.GetLast() > 0 {
+		return errors.New("only first or last can be used, not both")
+	}
+
+	if (request.GetFirst() > 0 && request.GetShowAll()) || (request.GetLast() > 0 && request.GetShowAll()) {
+		return errors.New("cannot use show all with first/last flags")
+	}
+
+	return nil
+}
+
+func (s *Server) followLogs(wg *sync.WaitGroup, options types.ContainerLogsOptions, stream pb.Centralog_GetLogsServer, request *pb.GetLogsRequest) {
+	options.Follow = true
+
+	if !request.ShowAll {
+		options.Since = "0m"
+	}
+
+	stopSignals := []chan struct{}{}
+
+	wg.Add(1)
+	for _, container := range request.GetContainers() {
+		stopSignal := make(chan struct{})
+		stopSignals = append(stopSignals, stopSignal)
+
+		logWriter := docker.NewServerLogWriter(stopSignal, container, stream)
+
+		go s.Docker.CollectLogs(container, logWriter, options)
+	}
+
+	// since the logs are being collected inside goroutines, we have to wait until
+	// the client closes the connection so the connection doesn't get instantly
+	// dropped by the server.
+	select {
+	case <-stream.Context().Done():
+		for _, signal := range stopSignals {
+			signal <- struct{}{}
+		}
+		wg.Done()
+	}
 }
