1a73a2933f4b11fd187b0a88ca7a02a20a4bf069
XiovV <xiovv@tutanota.com>
2023-01-10 13:33:04 +0100

    checking if the database file exists, generating it if it doesn't

--- repository/sqlite.go
+++ repository/sqlite.go
@@ -1,13 +1,15 @@
 package repository
 
 import (
+	"errors"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
+	"os"
 )
 
 const (
-	dbFileName = "db.db"
+	dbPath = "data/sqlite.db"
 )
 
 type SQLite struct {
@@ -15,10 +17,39 @@ type SQLite struct {
 }
 
 func New() *SQLite {
-	db, err := sqlx.Connect("sqlite3", dbFileName)
-	if err != nil {
-		log.Fatal(err)
+	if _, err := os.Stat("data"); errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir("data", os.ModePerm)
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
 
+	db, err := sqlx.Connect("sqlite3", dbPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	migrate(db)
+
 	return &SQLite{db: db}
+}
+
+func migrate(db *sqlx.DB) {
+	localTableSchema := `
+	CREATE TABLE IF NOT EXISTS local(
+	    api_key BLOB,
+	    containers TEXT
+	);
+`
+
+	logsSchema := `
+	CREATE TABLE IF NOT EXISTS logs(
+	    message TEXT,
+	    containerID TEXT,
+	    timestamp INTEGER
+	);
+`
+
+	db.MustExec(localTableSchema)
+	db.MustExec(logsSchema)
 }
